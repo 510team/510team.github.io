2914f9f5a59afc3d494a5f57815795b58a78ee55
fan zhang <fan.zhang1@nio.com>
2018-11-02 10:42:15 +0800

    添加favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,32 @@
 module.exports = {
-  title: "菜鸟教你一步一步读Vue源码",
-  description: "菜鸟教你一步一步读Vue源码",
-  base: "/",
-  themeConfig: {
-    displayAllHeaders: true, // 默认值：false
-    nav: [{ text: "首页", link: "/" }],
-    sidebar: [
-      {
-        title: "前情提要",
-        collapsable: false,
-        children: [
-          ["/foreword/", "介绍"],
-          ["/foreword/flow", "Flow"],
-          ["/foreword/rollup", "Rollup"]
+    title: "菜鸟教你一步一步读Vue源码",
+    description: "菜鸟教你一步一步读Vue源码",
+    base: "/",
+    head: [
+        ["link", { rel: "icon", href: `/favicon.ico` }],
+        ["meta", { name: "theme-color", content: "#3eaf7c" }],
+        ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+        [
+            "meta",
+            { name: "apple-mobile-web-app-status-bar-style", content: "black" }
         ]
-      }
-    ]
-  },
-  markdown: {
-    lineNumbers: true
-  }
+    ],
+    themeConfig: {
+        displayAllHeaders: true, // 默认值：false
+        nav: [{ text: "首页", link: "/" }],
+        sidebar: [
+            {
+                title: "前情提要",
+                collapsable: false,
+                children: [
+                    ["/foreword/", "介绍"],
+                    ["/foreword/flow", "Flow"],
+                    ["/foreword/rollup", "Rollup"]
+                ]
+            }
+        ]
+    },
+    markdown: {
+        lineNumbers: true
+    }
 };
