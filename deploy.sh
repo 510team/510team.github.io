fc7ac37e79b6ff3931b887051c784a36e7dee54e
fan zhang <fan.zhang1@nio.com>
2018-12-11 17:00:19 +0800

    添加deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,5 @@ git add -A
 git commit -m 'deploy'
 
 git push -f git@github.com:510team/510team.github.io.git master
+
+git push -f git@git.dev.tencent.com:dtid_01b28ab2fbe9686a/510team.git master
